a8ac0bf4c16013a38bec1f3c6afe66beb84e85e2
Osman Beyhan <104824448+osmn-byhn@users.noreply.github.com>
2025-09-15 02:56:09 +0300

    Update NavbarSticky.jsx

--- src/components/NavbarSticky.jsx
+++ src/components/NavbarSticky.jsx
@@ -30,7 +30,7 @@ export default function StickyNavbar() {
       <a href="https://github.com/osmn-byhn">
         <FontAwesomeIcon icon={faGithub} className="h-6 w-6 text-white" />
       </a>
-      <a href="https://www.instagram.com/osmn-byhn.vue/">
+      <a href="https://www.instagram.com/osmn-byhn/">
         <FontAwesomeIcon icon={faInstagram} className="h-6 w-6 text-white" />
       </a>
       <a href="https://www.linkedin.com/in/osman-beyhan-12304b23a">
